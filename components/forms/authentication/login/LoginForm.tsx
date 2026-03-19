c887944ae0645940b1ca001fd88c769a50abab2d
TremerYT <jefferymutuku283@gmail.com>
2026-03-19 21:29:26 +0300

    Changes Made: - Created LoginForm.tsx

---
+++ components/forms/authentication/login/LoginForm.tsx
@@ -0,0 +1,83 @@
+import {Button, Checkbox, Col, Form, Input, Row} from "antd";
+import {FcGoogle} from "react-icons/fc";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
+
+type FieldType = {
+	username: string;
+	password: string;
+	remember?: boolean;
+}
+
+const LoginForm = () => {
+	const [form] = Form.useForm<FieldType>();
+	const [loading, setIsLoading] = useState(false);
+	const router = useRouter();
+	return (
+		<Form
+			form={form}
+			layout="vertical"
+			requiredMark={false}
+			className="w-1/2"
+		>
+			<Form.Item
+				name="userName"
+				label="Username"
+				rules={[{required: true, message: "Username is required"}]}
+			>
+				<Input size="large" className="w-1/2 mx-auto"/>
+			</Form.Item>
+			<Form.Item
+				name="password"
+				label="Password"
+				rules={[{required: true, message: "Password is required"}]}
+			>
+				<Input size="large"/>
+			</Form.Item>
+			
+			<Form.Item name="remember" valuePropName="checked">
+				<Row justify="space-between">
+					<Col span={12}>
+						<Checkbox>Remember Me</Checkbox>
+					</Col>
+					<Col span={12} className="text-right">
+						<Button type="link" size="large" onClick={() => router.push("/auth/forgot_password")}>
+							Forgot Password?
+						</Button>
+					</Col>
+				</Row>
+			</Form.Item>
+			<div className="flex flex-col gap-4 mb-2">
+				<Button
+					onClick={() => {
+					}}
+					htmlType="submit"
+					loading={loading}
+					className="bg-black! text-white! px-20 py-8! rounded-2xl! font-bold! w-full"
+				>
+					Sign in
+				</Button>
+				
+				<Button
+					icon={<FcGoogle/>}
+					loading={loading}
+					className="px-20 py-8! rounded-2xl! font-bold! w-full"
+				>
+					Sign in with google
+				</Button>
+			</div>
+			<div className="flex items-center justify-center">
+				<p>Dont have an account?</p>
+				<Button
+					type="link"
+					size="large"
+					onClick={() => router.push("/auth/register")}
+				>
+					Sign up
+				</Button>
+			</div>
+		</Form>
+	);
+}
+
+export default LoginForm
