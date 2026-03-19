74f762e9a5fa7a15e8ca2fae44fa4b04b434b6cb
TremerYT <jefferymutuku283@gmail.com>
2026-03-19 21:27:25 +0300

    Changes Made: - Created the types page to import types

---
+++ types/FormTypes.ts
@@ -0,0 +1,15 @@
+export type RegisterFormType = {
+	businessName: string;
+	phone: string;
+	companyEmail: string;
+	city: string;
+	zipCode: string;
+	address: string;
+	terms: boolean;
+	fullName: string;
+	userName: string;
+	email: string;
+	userPhoneNumber: string;
+	password: string;
+	otp?: string;
+}
