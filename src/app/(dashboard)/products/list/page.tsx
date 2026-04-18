8b27f8c2168ef640b9ddadd09989786d9b5116be
TremerYT <jefferymutuku283@gmail.com>
2026-04-18 20:27:55 +0300

    Changes Made: - Created the following pages

---
+++ src/app/(dashboard)/products/list/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {Button, Typography} from "antd";
+import {FileExcelFilled, FilePdfFilled, PlusOutlined, ReloadOutlined} from "@ant-design/icons";
+import ProductsTable from "../../../../../components/tables/ProductsTable";
+
+const {Title, Text} = Typography;
+
+const ListProducts = () => {
+	return (
+		<>
+			<div className="flex justify-between items-center mb-4">
+				<div>
+					<Title level={2} className="m-0!">Product List</Title>
+					<Text>Manage your products</Text>
+				</div>
+				<div className="flex gap-3">
+					<Button
+						type="text"
+						icon={<FilePdfFilled style={{fontSize: 23, color: "red"}}/>}
+					/>
+					<Button
+						type="text"
+						icon={<FileExcelFilled style={{fontSize: 23, color: "green"}}/>}
+					/>
+					<Button
+						type="text"
+						icon={<ReloadOutlined style={{fontSize: 23}}/>}
+					/>
+					<Button
+						type="primary"
+						icon={<PlusOutlined/>}
+						size={"medium"}
+					>
+						Add Product
+					</Button>
+				</div>
+			</div>
+			<ProductsTable/>
+		</>
+	);
+}
+export default ListProducts;
---
+++ src/app/(dashboard)/products/units/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {Button, Typography} from "antd";
+import {PlusOutlined, ReloadOutlined} from "@ant-design/icons";
+import UnitsTable from "../../../../../components/tables/UnitsTable";
+
+const {Title, Text} = Typography;
+const UnitsPage = () => {
+	return (
+		<>
+			<div className="flex justify-between items-center mb-4">
+				<div>
+					<Title level={2} className="m-0!">Product Units</Title>
+					<Text>Manage your product units</Text>
+				</div>
+				<div className="flex gap-3">
+					<Button
+						type="text"
+						icon={<ReloadOutlined style={{fontSize: 23}}/>}
+					/>
+					<Button
+						type="primary"
+						icon={<PlusOutlined/>}
+						size={"medium"}
+					>
+						Add Unit
+					</Button>
+				</div>
+			</div>
+			<UnitsTable/>
+		</>
+	);
+}
+
+export default UnitsPage
